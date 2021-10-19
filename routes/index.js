6101fea40da669c1b99aead66d9aa4dc916e54af
Vakha <gvardee95@gmail.com>
2021-10-19 11:04:57 +0300

    routes added

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,11 @@ const { Router } = require("express");
 
 const router = Router();
 
+router.use("/users", require("./users.route"));
+router.use("/products", require("./products.route"));
+router.use("/brands", require("./brands.route"));
+router.use("/reviews", require("./reviews.route"));
+router.use("/cart", require("./cart.route"));
+router.use("/categories", require("./categories.route"));
+
 module.exports = router;
