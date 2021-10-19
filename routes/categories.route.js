96c55be8fd3ed65320125bb92d7b7cd3864b6c93
Satuev <alitm.s@mail.ru>
2021-10-19 11:48:29 +0300

    изменил роуты для категорий и брэнда

--- routes/categories.route.js
+++ routes/categories.route.js
@@ -3,9 +3,9 @@ const { categoriesController } = require("../controllers/categories.controller")
 
 const router = Router()
 
-router.post("/admin", categoriesController.addCategory)
+router.post("/admin/add", categoriesController.addCategory)
 router.get("/", categoriesController.getAllCategories)
-router.patch("/:id/admin", categoriesController.updateCategory)
-router.delete("/:id/admin", categoriesController.deleteCategory)
+router.patch("/admin/category/:id", categoriesController.updateCategory)
+router.delete("/admin/category/:id", categoriesController.deleteCategory)
 
 module.exports = router
